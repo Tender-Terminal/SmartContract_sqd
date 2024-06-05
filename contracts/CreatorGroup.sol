1016b851c7b76de3dedfdd14eb9bc6bf9819776b
Director-Junior <danielsolis940514@gmail.com>
2024-06-05 01:31:53 -0500

    ReentrancyGuard import route fixed

--- contracts/CreatorGroup.sol
+++ contracts/CreatorGroup.sol
@@ -9,7 +9,7 @@ import "./interfaces/IContentNFT.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 
 contract CreatorGroup is Initializable, ICreatorGroup, ReentrancyGuard {
--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -6,7 +6,8 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/ICreatorGroup.sol";
 import "./interfaces/IContentNFT.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
 
 contract Marketplace is ReentrancyGuard {
     // Enum defining different sale types
